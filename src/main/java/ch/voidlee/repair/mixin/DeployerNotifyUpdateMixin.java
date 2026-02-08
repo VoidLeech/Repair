e0993d26432660dd1fb39d16a6a571c7533570f6
VoidLeech <ian.gerritse@gmail.com>
2026-02-08 13:15:43 +0100

    build: interface mixins powered by mixinbooster, coming to a mod near you

--- src/main/java/ch/voidlee/repair/mixin/DeployerNotifyUpdateMixin.java
+++ src/main/java/ch/voidlee/repair/mixin/DeployerNotifyUpdateMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 // https://github.com/Creators-of-Create/Create/commit/dd133f667969870dd6e9b96395097ac93cd91ced
 @Mixin(DeployerBlock.class)
 public class DeployerNotifyUpdateMixin {
-    @WrapOperation(method = "lambda$use$1", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/kinetics/deployer/DeployerBlockEntity;sendData()V"))
+    @WrapOperation(method = "lambda$use$1", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/kinetics/deployer/DeployerBlockEntity;sendData()V"), remap = false)
     private static void create_repair$actuallyJustNotifyUpdateInstead(DeployerBlockEntity instance, Operation<Void> original) {
         instance.setChanged();
         original.call(instance);
