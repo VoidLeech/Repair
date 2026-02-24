ef4f90a780c3f8a0fbd1157c6668d017ab1e1a43
vercte <lopstedener@gmail.com>
2026-02-24 14:41:33 -0800

    log Create Repair initialization

--- src/main/java/ch/voidlee/repair/Repair.java
+++ src/main/java/ch/voidlee/repair/Repair.java
@@ -2,16 +2,16 @@ package ch.voidlee.repair;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(Repair.MOD_ID)
 public class Repair {
+    public static final String NAME = "Create Repair";
     public static final String MOD_ID = "create_repair";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public Repair()
-    {
-
+    public Repair() {
+        LOGGER.info("{} {} initializing! Commit hash: {}", NAME, RepairBuildInfo.VERSION, RepairBuildInfo.GIT_COMMIT);
+        LOGGER.info("{} modifies Create heavily to fix bugs! This should be regarded to any log-consumers.", NAME);
     }
 }
