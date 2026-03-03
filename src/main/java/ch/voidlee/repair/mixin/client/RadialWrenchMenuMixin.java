530827c48b520471172d643a28dc73bd73ccff8f
vercte <me@vercte.net>
2026-03-03 14:13:57 -0800

    Backport #9608's fix

---
+++ src/main/java/ch/voidlee/repair/mixin/client/RadialWrenchMenuMixin.java
@@ -0,0 +1,24 @@
+package ch.voidlee.repair.mixin.client;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.simibubi.create.content.contraptions.wrench.RadialWrenchMenu;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+// https://github.com/Creators-of-Create/Create/issues/9608
+@Mixin(RadialWrenchMenu.class)
+public class RadialWrenchMenuMixin {
+    @WrapOperation(method = "renderRadialSectors", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BlockEntity;getLevel()Lnet/minecraft/world/level/Level;"))
+    public Level maybeThereIsNoLevel(BlockEntity blockEntity, Operation<Level> original) {
+        return blockEntity != null ? original.call(blockEntity) : null;
+    }
+
+    @WrapWithCondition(method = "renderRadialSectors", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/entity/BlockEntity;setLevel(Lnet/minecraft/world/level/Level;)V"))
+    public boolean onlyIfTheBlockEntityExists(BlockEntity blockEntity, Level level) {
+        return blockEntity != null;
+    }
+}
