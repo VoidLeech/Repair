930eb8ef0521ba5754105cfeb74b641abb23530e
vercte <me@vercte.net>
2026-03-04 11:40:03 -0800

    Backport efc3d3e
    Fix copycat blocks dropping items when /fill'd
    Co-authored-by: IThundxr <me@ithundxr.dev>

---
+++ src/main/java/ch/voidlee/repair/mixin/CopycatBlockEntityMixin.java
@@ -0,0 +1,25 @@
+package ch.voidlee.repair.mixin;
+
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.decoration.copycat.CopycatBlockEntity;
+import net.minecraft.world.Clearable;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+// https://github.com/Creators-of-Create/Create/commit/efc3d3e
+@Mixin(CopycatBlockEntity.class)
+public class CopycatBlockEntityMixin implements Clearable {
+    @Shadow
+    private BlockState material;
+
+    @Shadow
+    private ItemStack consumedItem;
+
+    @Override
+    public void clearContent() {
+        this.material = AllBlocks.COPYCAT_BASE.getDefaultState();
+        this.consumedItem = ItemStack.EMPTY;
+    }
+}
