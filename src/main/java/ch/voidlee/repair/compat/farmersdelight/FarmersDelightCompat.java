2843f1f5f8ad3b20aa8793b7209a3bf4c579bea6
vercte <me@vercte.net>
2026-03-04 14:00:41 -0800

    add comments for PR #9940

--- src/main/java/ch/voidlee/repair/compat/farmersdelight/FarmersDelightCompat.java
+++ src/main/java/ch/voidlee/repair/compat/farmersdelight/FarmersDelightCompat.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.Level;
 
 import vectorwing.farmersdelight.common.registry.ModBlocks;
 
+// https://github.com/Creators-of-Create/Create/pull/9940
 public class FarmersDelightCompat {
     public static boolean shouldHarvestMushroom(Level world, BlockPos pos) {
         return !world.getBlockState(pos.below()).is(ModBlocks.RICH_SOIL.get());
--- src/main/java/ch/voidlee/repair/mixin/HarvesterMovementBehaviourMixin.java
+++ src/main/java/ch/voidlee/repair/mixin/HarvesterMovementBehaviourMixin.java
@@ -12,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+// https://github.com/Creators-of-Create/Create/pull/9940
 @Mixin(HarvesterMovementBehaviour.class)
 public class HarvesterMovementBehaviourMixin {
     @Inject(method = "isValidOther", at = @At("HEAD"), cancellable = true, remap = false)
