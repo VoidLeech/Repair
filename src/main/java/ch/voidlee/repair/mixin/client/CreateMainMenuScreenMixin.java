c08158bb613dba8ab41cdf364e00117bbc43370b
vercte <me@vercte.net>
2026-03-05 19:44:03 -0800

    Link "Report Issues" to "Reporting Issues to Create"

--- src/main/java/ch/voidlee/repair/mixin/client/CreateMainMenuScreenMixin.java
+++ src/main/java/ch/voidlee/repair/mixin/client/CreateMainMenuScreenMixin.java
@@ -3,44 +3,28 @@ package ch.voidlee.repair.mixin.client;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.infrastructure.gui.CreateMainMenuScreen;
-import net.createmod.catnip.lang.FontHelper;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.AbstractWidget;
-import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreateMainMenuScreen.class)
 public abstract class CreateMainMenuScreenMixin extends Screen {
     @Unique
-    private AbstractWidget create_repair$create_report_issues_button;
+    private final String CREATE_REPAIR_HOW_TO_REPORT_ISSUES = "https://github.com/VoidLeech/Repair/wiki/Reporting-Issues-to-Create";
+
+    @Shadow
+    protected abstract void linkTo(String url);
 
     protected CreateMainMenuScreenMixin(Component pTitle) {
         super(pTitle);
     }
 
-    @WrapOperation(method = "addButtons", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/infrastructure/gui/CreateMainMenuScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", ordinal = 5))
-    private GuiEventListener create_repair$storeAndDisableReportIssues(CreateMainMenuScreen screen, GuiEventListener button, Operation<GuiEventListener> original) {
-        if(button instanceof AbstractWidget widget) {
-            create_repair$create_report_issues_button = widget;
-            widget.active = false;
-        }
-        return original.call(screen, button);
-    }
-
-    @Inject(method = "renderWindowForeground", at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V"), remap = false)
-    private void create_repair$renderIssuesTooltip(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
-        if (mouseX < create_repair$create_report_issues_button.getX() || mouseX > create_repair$create_report_issues_button.getX() + 98)
-            return;
-        if (mouseY < create_repair$create_report_issues_button.getY() || mouseY > create_repair$create_report_issues_button.getY() + 20)
-            return;
-        graphics.renderComponentTooltip(font,
-                FontHelper.cutTextComponent(Component.translatable("create_repair.menu.unsupported"), FontHelper.Palette.ALL_GRAY), mouseX,
-                mouseY);
+    @WrapOperation(method = "addButtons", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/Button;builder(Lnet/minecraft/network/chat/Component;Lnet/minecraft/client/gui/components/Button$OnPress;)Lnet/minecraft/client/gui/components/Button$Builder;", ordinal = 3))
+    private Button.Builder create_repair$changeReportIssuesLink(Component message, Button.OnPress ogOnPress, Operation<Button.Builder> original) {
+        return original.call(message, (Button.OnPress)($ -> linkTo(CREATE_REPAIR_HOW_TO_REPORT_ISSUES)));
     }
 }
