48f6fcc78391c117bac3421c31c0882995a9611b
VoidLeech <ian.gerritse@gmail.com>
2026-03-07 23:21:54 +0100

    fix: funnel extraction crashing

--- src/main/java/ch/voidlee/repair/mixin/FunnelBlockEntityMixin.java
+++ src/main/java/ch/voidlee/repair/mixin/FunnelBlockEntityMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.lang.ref.WeakReference;
@@ -44,5 +45,10 @@ public abstract class FunnelBlockEntityMixin {
                 return;
             lastObserved = null;
         }
+    }
+
+    @ModifyArg(method = "activateExtractor", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getEntities(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/AABB;)Ljava/util/List;"), index = 1, remap = false)
+    private AABB create_repair$thereIsAnAABB(AABB original) {
+        return getEntityOverflowScanningArea();
     }
 }
