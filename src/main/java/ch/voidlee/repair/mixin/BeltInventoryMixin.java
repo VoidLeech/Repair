dbe901b6ee175d26680fdc9277b6039e2693ee30
VoidLeech <ian.gerritse@gmail.com>
2026-03-18 02:48:32 +0100

    Backport #10017 diff #9954, closes #2
    Co-authored-by: Apertyotis <1947517504@qq.com>

---
+++ src/main/java/ch/voidlee/repair/mixin/BeltInventoryMixin.java
@@ -0,0 +1,44 @@
+package ch.voidlee.repair.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.simibubi.create.content.kinetics.belt.transport.BeltInventory;
+import com.simibubi.create.content.kinetics.belt.transport.TransportedItemStack;
+import net.minecraft.nbt.CompoundTag;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+
+// The diff of these diffs
+// https://github.com/Creators-of-Create/Create/pull/9954
+// https://github.com/Creators-of-Create/Create/pull/10017
+@Mixin(BeltInventory.class)
+public abstract class BeltInventoryMixin {
+    @Shadow
+    @Final
+    List<TransportedItemStack> toInsert;
+
+    @Shadow
+    @Final
+    List<TransportedItemStack> toRemove;
+
+    @Shadow
+    protected abstract void insert(TransportedItemStack newStack);
+
+    @Shadow
+    @Final
+    private List<TransportedItemStack> items;
+
+    @WrapMethod(method = "write", remap = false)
+    private CompoundTag create_repair$fixBeltDupe(Operation<CompoundTag> original) {
+        if (!toInsert.isEmpty() || !toRemove.isEmpty()) {
+            toInsert.forEach(this::insert);
+            toInsert.clear();
+            items.removeAll(toRemove);
+            toRemove.clear();
+        }
+        return original.call();
+    }
+}
