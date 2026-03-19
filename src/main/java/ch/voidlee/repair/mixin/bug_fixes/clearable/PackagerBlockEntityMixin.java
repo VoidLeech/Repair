dbdb6e3fb3359290c68b74e2c2223da648fcc643
VoidLeech <ian.gerritse@gmail.com>
2026-03-19 13:56:30 +0100

    fix: transaction safety

--- src/main/java/ch/voidlee/repair/mixin/bug_fixes/clearable/PackagerBlockEntityMixin.java
+++ src/main/java/ch/voidlee/repair/mixin/bug_fixes/clearable/PackagerBlockEntityMixin.java
@@ -3,6 +3,7 @@ package ch.voidlee.repair.mixin.bug_fixes.clearable;
 import com.simibubi.create.content.logistics.BigItemStack;
 import com.simibubi.create.content.logistics.packager.PackagerBlockEntity;
 import com.simibubi.create.content.logistics.packager.PackagerItemHandler;
+import io.github.fabricators_of_create.porting_lib.transfer.TransferUtil;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.world.Clearable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,7 +22,7 @@ public abstract class PackagerBlockEntityMixin implements Clearable {
     @Override
     public void clearContent() {
         if  (!inventory.isResourceBlank()) {
-            try (Transaction transaction = Transaction.openOuter()) {
+            try (Transaction transaction = TransferUtil.getTransaction()) {
                 inventory.extract(inventory.getResource(), 1, transaction);
                 transaction.commit();
             }
