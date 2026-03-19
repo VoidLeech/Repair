1ac1b7ef7a2fce5656b8c5b58aced9163268084f
VoidLeech <ian.gerritse@gmail.com>
2026-03-19 12:18:19 +0100

    chore: split up mixins so they can confirm to organization

--- src/main/java/ch/voidlee/repair/mixin/bug_fixes/MechanicalMixerBlockEntityMixin.java
+++ src/main/java/ch/voidlee/repair/mixin/bug_fixes/MechanicalMixerBlockEntityMixin.java
@@ -1,7 +1,5 @@
-package ch.voidlee.repair.mixin;
+package ch.voidlee.repair.mixin.bug_fixes;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.simibubi.create.content.kinetics.mixer.MechanicalMixerBlockEntity;
 import com.simibubi.create.content.processing.basin.BasinOperatingBlockEntity;
 import net.minecraft.core.BlockPos;
@@ -14,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 // https://github.com/Creators-of-Create/Create/pull/9921
-// https://github.com/Creators-of-Create/Create/pull/10022
 @Mixin(MechanicalMixerBlockEntity.class)
 public abstract class MechanicalMixerBlockEntityMixin extends BasinOperatingBlockEntity {
     @Shadow
@@ -32,13 +29,5 @@ public abstract class MechanicalMixerBlockEntityMixin extends BasinOperatingBloc
             else if (runningTicks == 20)
                 runningTicks++;
         }
-    }
-
-    @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;clamp(III)I", remap = true), remap = false)
-    private int create_repair$mixersMayGoSlower(int pValue, int pMin, int pMax, Operation<Integer> original) {
-        if (pValue < pMax) {
-            return original.call(pValue, pMin, pMax);
-        }
-        return Math.max(pValue, pMin);
     }
 }
---
+++ src/main/java/ch/voidlee/repair/mixin/crash_fixes/LecternControllerBlockEntityMixin.java
@@ -0,0 +1,24 @@
+package ch.voidlee.repair.mixin.crash_fixes;
+
+import com.simibubi.create.content.redstone.link.controller.LecternControllerBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.ItemStackHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+// https://github.com/Creators-of-Create/Create/issues/9448
+@Mixin(LecternControllerBlockEntity.class)
+public abstract class LecternControllerBlockEntityMixin {
+    @Shadow
+    private ItemStackHandler controllerFreq;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void create_repair$initializeHandler(BlockEntityType<?> type, BlockPos pos, BlockState state, CallbackInfo ci) {
+        this.controllerFreq = new ItemStackHandler(12);
+    }
+}
