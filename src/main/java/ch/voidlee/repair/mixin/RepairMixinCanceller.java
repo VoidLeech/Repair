a3b5aa6c9a79306db96b23043661247a88126166
VoidLeech <ian.gerritse@gmail.com>
2026-07-05 21:42:03 +0200

    fix: don't break older JM versions, set broken betas as incompatible

--- src/main/java/ch/voidlee/repair/mixin/RepairMixinCanceller.java
+++ src/main/java/ch/voidlee/repair/mixin/RepairMixinCanceller.java
@@ -1,6 +1,8 @@
 package ch.voidlee.repair.mixin;
 
 import com.bawnorton.mixinsquared.api.MixinCanceller;
+import net.minecraftforge.fml.loading.LoadingModList;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
 
 import java.util.List;
 
@@ -9,8 +11,11 @@ public class RepairMixinCanceller implements MixinCanceller {
 
     public RepairMixinCanceller() {
         if (RepairMixinPlugin.isModEarlyLoaded("journeymap")) {
-            // Todo: version check
-            shouldCancelJourneymapMixin = true;
+            ArtifactVersion version = LoadingModList.get().getModFileById("journeymap").getMods().get(0).getVersion();
+            // Older versions aren't prefixed with the Minecraft version so this is straight up simpler than the necessary dependency range in mods.toml
+            if (version.toString().contains("1.20.1")) {
+                shouldCancelJourneymapMixin = true;
+            }
         }
     }
 
